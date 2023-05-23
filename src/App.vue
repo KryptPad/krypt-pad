f48cfb63b9ad03452463ab2b431a92086c87164e
Eric Butler <ebutler@striven.com>
2023-05-23 00:56:25 -0400

    Trying navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <v-app>
-
     <v-layout class="h-100">
       <v-main class="h-100">
 
 
-        <v-flex class="d-flex flex-column h-100">
+        <div class="d-flex flex-column h-100">
 
           <title-bar></title-bar>
-
           <v-toolbar density="compact">
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
@@ -17,7 +15,7 @@
             <v-spacer></v-spacer>
 
             <v-btn icon>
-              <v-icon>mdi-magnify</v-icon>
+              <v-icon>mdi-folder-open-outline</v-icon>
             </v-btn>
 
             <v-btn icon>
@@ -29,25 +27,49 @@
             </v-btn>
           </v-toolbar>
 
-          <v-tabs>
-            <v-tab key="doc1">Document 1</v-tab>
-            <v-tab key="doc2">Item Two</v-tab>
-            <v-tab>Item Three</v-tab>
-          </v-tabs>
+          <div class="d-flex">
 
-          <v-tabs-items class="flex-fill">
-            <v-tab-item key="doc1" class="h-100 d-block">
-              <textarea class="w-100 h-100"></textarea>
-            </v-tab-item>
+            <v-navigation-drawer :rail="true" permanent :absolute="false">
+              <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" title="John Leider" nav>
+                <!-- <template v-slot:append>
+                  <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
+                </template> -->
+              </v-list-item>
 
-            <v-tab-item key="doc2" class="h-100 d-block">
-              <textarea class="w-100 h-100"></textarea>
-            </v-tab-item>
+              <v-divider></v-divider>
 
-          </v-tabs-items>
+              <v-list density="compact" nav>
+                <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
+                <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+                <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+              </v-list>
+            </v-navigation-drawer>
 
-          
-        </v-flex>
+
+            <v-tabs>
+              <v-tab key="doc1">Document 1</v-tab>
+              <v-tab key="doc2">Item Two</v-tab>
+              <v-tab>Item Three</v-tab>
+            </v-tabs>
+
+            <v-tabs-items class="flex-fill">
+              <v-tab-item key="doc1" class="h-100 d-block">
+                <textarea class="w-100 h-100"></textarea>
+              </v-tab-item>
+
+              <v-tab-item key="doc2" class="h-100 d-block">
+                <textarea class="w-100 h-100"></textarea>
+              </v-tab-item>
+
+            </v-tabs-items>
+
+
+          </div>
+
+
+
+
+        </div>
       </v-main>
     </v-layout>
   </v-app>
@@ -81,5 +103,4 @@ textarea {
   padding: 1rem;
   color: #fff;
 }
-
 </style>
